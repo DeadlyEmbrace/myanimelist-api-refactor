90fa0e20a3e280c44f8deaa2ea29eff243232280
Chris Legault <chrislegault2011@gmail.com>
2013-07-22 12:06:44 -0300

    Manga refactorings - breaking down parsing logic into smaller methods to reduce complexity

--- app/lib/scrapers/manga_list_scraper.rb
+++ app/lib/scrapers/manga_list_scraper.rb
@@ -7,25 +7,30 @@ class MangaListScraper
       manga_list_xml = Nokogiri::XML.parse(xml)
       manga_list = MangaList.new
 
-      manga_list.manga = manga_list_xml.search('manga').map do |manga_node|
-        manga = Manga.new
-        manga.id = manga_node.at('series_mangadb_id').text.to_i
-        manga.title = manga_node.at('series_title').text
-        manga.type = manga_node.at('series_type').text
-        manga.status = manga_node.at('series_status').text
-        manga.chapters = manga_node.at('series_chapters').text.to_i
-        manga.volumes = manga_node.at('series_volumes').text.to_i
-        manga.image_url = manga_node.at('series_image').text
-        manga.listed_manga_id = manga_node.at('my_id').text.to_i
-        manga.volumes_read = manga_node.at('my_read_volumes').text.to_i
-        manga.chapters_read = manga_node.at('my_read_chapters').text.to_i
-        manga.score = manga_node.at('my_score').text.to_i
-        manga.read_status = manga_node.at('my_status').text
 
-        manga
+      manga_list_xml.search('manga').each do |manga_node|
+        manga_list.manga << self.parse_manga(manga_node)
       end
     end
 
     manga_list
   end
+
+  private
+    def self.parse_manga(manga_node)
+      manga = Manga.new
+      manga.id = manga_node.at('series_mangadb_id').text.to_i
+      manga.title = manga_node.at('series_title').text
+      manga.type = manga_node.at('series_type').text
+      manga.status = manga_node.at('series_status').text
+      manga.chapters = manga_node.at('series_chapters').text.to_i
+      manga.volumes = manga_node.at('series_volumes').text.to_i
+      manga.image_url = manga_node.at('series_image').text
+      manga.listed_manga_id = manga_node.at('my_id').text.to_i
+      manga.volumes_read = manga_node.at('my_read_volumes').text.to_i
+      manga.chapters_read = manga_node.at('my_read_chapters').text.to_i
+      manga.score = manga_node.at('my_score').text.to_i
+      manga.read_status = manga_node.at('my_status').text
+      manga
+    end
 end
