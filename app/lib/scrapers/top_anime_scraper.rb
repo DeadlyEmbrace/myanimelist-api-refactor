5f81cbbfdb620caf0e30e5a7d40c909293f28ddb
Chris Legault <chrislegault2011@gmail.com>
2013-07-23 08:47:22 -0300

    Top Anime - reduced complexity

--- app/lib/scrapers/top_anime_scraper.rb
+++ app/lib/scrapers/top_anime_scraper.rb
@@ -3,41 +3,55 @@ class TopAnimeScraper
     top_anime_page = Nokogiri::HTML(html)
     top_anime = []
 
-    top_anime_page.search('div#content table tr').each do |results_row|
-      anime_title_node = results_row.at('td a strong')
+    top_anime_page.search('div#content table tr').each do |search_result|
+      anime_title_node = search_result.at('td a strong')
       next unless anime_title_node
       anime_url = anime_title_node.parent['href']
       next unless anime_url
       anime_url.match %r{http://myanimelist.net/anime/(\d+)/?.*}
-
-      anime = Anime.new
-      anime.id = $1.to_i
-      anime.title = anime_title_node.text
-
-      table_cell_nodes = results_row.search('td')
-      content_cell = table_cell_nodes.at('div.spaceit_pad')
-
-      members_cell = content_cell.at('span.lightLink')
-      members = members_cell.text.strip.gsub!(/\D/, '').to_i
-      members_cell.remove
-
-      stats = content_cell.text.strip.split(',')
-      type = stats[0]
-      episodes = stats[1].gsub!(/\D/, '')
-      members_score = stats[2].match(/\d+(\.\d+)?/).to_s.to_f
-
-      anime.type = type
-      anime.episodes = episodes.to_i unless episodes.size <= 0
-      anime.members_count = members
-      anime.members_score = members_score
-
-      if (image_node = results_row.at('td a img'))
-        anime.image_url = image_node['src']
-      end
-
-      top_anime << anime
+      top_anime << self.parse_anime($1, search_result, anime_title_node)
     end
 
     top_anime
   end
+
+  private
+
+    def self.parse_anime(id, search_result, anime_title_node)
+      table_cell_nodes = search_result.search('td')
+      content_cell = table_cell_nodes.at('div.spaceit_pad')
+      members_count = self.parse_members_count(content_cell.at('span.lightLink'))
+      stats = content_cell.text.strip.split(',')
+
+      Anime.new({
+        id: id.to_i,
+        title: anime_title_node.text,
+        type: stats[0],
+        image_url: self.parse_image_url(search_result),
+        members_count: members_count,
+        members_score: stats[2].match(/\d+(\.\d+)?/).to_s.to_f,
+        episodes: self.parse_episodes(stats[1])
+      })
+    end
+
+    def self.parse_image_url(search_result)
+      image_url = nil
+
+      if (image_node = search_result.at('td a img'))
+        image_url = image_node['src']
+      end
+
+      image_url
+    end
+
+    def self.parse_members_count(members_cell)
+      members = members_cell.text.strip.gsub!(/\D/, '').to_i
+      members_cell.remove
+      members
+    end
+
+    def self.parse_episodes(episode_stats)
+      episodes = episode_stats.gsub!(/\D/, '')
+      episodes.to_i unless episodes.size <= 0
+    end
 end
